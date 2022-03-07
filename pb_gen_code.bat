19765fcc973c841934a67de90496ae378766e517
xuchang <xuchang@boke.com>
2022-03-07 22:52:42 +0800

    modify pb_gen_code script

--- pb_gen_code.bat
+++ pb_gen_code.bat
@@ -1,12 +1,3 @@
 cd .\protocol
 
-.\protoc.exe --cpp_out=.\ db_data.proto
-
-.\protoc.exe --cpp_out=.\ server_base.proto
-.\protoc.exe --cpp_out=.\ server_common.proto
-
-.\protoc.exe --cpp_out=.\ server_db.proto
-
-
-.\protoc.exe --cpp_out=.\ client_base.proto
-.\protoc.exe --cpp_out=.\ client_common.proto
+.\protoc.exe --cpp_out=.\ *.proto
--- pb_gen_code.sh
+++ pb_gen_code.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 cd protocol
 
-protoc --cpp_out=./ ./db_data.proto
-
-protoc --cpp_out=./ ./server_base.proto
-protoc --cpp_out=./ ./server_common.proto
-
-protoc --cpp_out=./ ./server_db.proto
-
-protoc --cpp_out=./ ./client_base.proto
-protoc --cpp_out=./ ./client_common.proto
+protoc --cpp_out=./ `find ./ -name "*proto"`
