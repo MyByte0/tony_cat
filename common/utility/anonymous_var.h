558279379d67e3f74094ae3ca05935642faa6a77
“mybyte” <my_byte@outlook.com>
2023-02-15 20:59:06 +0800

    add anonymous_var define

---
+++ common/utility/anonymous_var.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_UTILITY_ANONYMOUS_VAR_H_
+#define COMMON_UTILITY_ANONYMOUS_VAR_H_
+
+#ifndef _
+
+#define _PLACEHOLDER_COMB(x, y) x##y
+#define _PLACEHOLDER(x, y) _PLACEHOLDER_COMB(x, y)
+
+#define _  _PLACEHOLDER(_PlaceholderVar, __COUNTER__)
+
+#endif
+
+#endif // COMMON_UTILITY_ANONYMOUS_VAR_H_
